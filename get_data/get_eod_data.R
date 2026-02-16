5e7c6f98e0344d98ef921e7d89f1845b6ada4d68
Corey Teply <teplycj@gmail.com>
2026-02-16 09:11:26 -0800

    moving EOD api functions to central script

--- get_data/get_eod_data.R
+++ get_data/get_eod_data.R
@@ -1,44 +1,14 @@
 
+source('~/stocks-eoddata/eod_helper_functions.R')
+
 # set constants -----------------------------------------------------------
 
-API_KEY <- paste0('?apiKey=', Sys.getenv('EOD_API_KEY'))
-EOD_URL <- 'https://api.eoddata.com/'
 BUFFER_TIME <- 10L # one call every ~10 seconds (given they only allow ~10 calls/minute for free version)
 WEEKEND_DAYS <- c('Saturday', 'Sunday')
 OUTPUT_DIR <- '~/local_stocks/'
 OUTPUT_FILE_NAME <- 'stock_data.csv'
 
 # helper functions --------------------------------------------------------
-
-build_httr_data <- function(httr_content) {
-  lapply(httr_content, as.data.frame) |>
-    data.table::rbindlist(use.names = TRUE, fill = TRUE)
-}
-
-get_eod_data <- function(url_ext, time_stamp = NULL) {
-  query_url <- paste0(
-    EOD_URL, url_ext, API_KEY
-  )
-  if (!is.null(time_stamp)) {
-    query_url <- paste0(query_url, '&dateStamp=', as.character(time_stamp))
-  }
-  response_val <- httr::GET(
-    url = query_url, 
-    httr::content_type("application/octet-stream")
-  )
-  tryCatch(
-    {
-      httr::stop_for_status(response_val)
-      httr::content(response_val) |>
-        build_httr_data()
-    },
-    error = function(cond) {
-      message('No data for given URL. skipping...')
-      message(cond)
-      data.frame()
-    }
-  )
-}
 
 download_stock_data <- function(exchange_vec, date_vec, buffer_time_constraint = FALSE) {
   ex_grid <- expand.grid(
